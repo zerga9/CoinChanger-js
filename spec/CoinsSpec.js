682aa293a406cc43bdf70fde6d828aed4fe29597
Gadiza Zerari <gadizaz@hotmail.com>
2018-06-13 23:20:35 +0100

    more tests for single coins passing

--- spec/CoinsSpec.js
+++ spec/CoinsSpec.js
@@ -8,5 +8,14 @@ describe("Coins", function() {
   it("should give 2 pence for 2", function() {
     expect(coins.change(2)).toEqual([2]);
   })
+  it("should give 5 pence for 5", function() {
+    expect(coins.change(5)).toEqual([5]);
+  })
+  it("should give 10 pence for 10", function() {
+    expect(coins.change(10)).toEqual([10]);
+  })
+  it("should give 20 pence for 20", function() {
+    expect(coins.change(20)).toEqual([20]);
+  })
 
 })
--- src/Coins.js
+++ src/Coins.js
@@ -3,9 +3,18 @@ function Coins(){
 };
 Coins.prototype.change = function(amount) {
   if(amount === 1) {
-    return [1]
+    return [1];
   }
   if(amount === 2) {
-    return [2]
+    return [2];
+  }
+  if(amount === 5) {
+    return [5];
+  }
+  if(amount === 10) {
+    return [10];
+  }
+  if(amount === 20) {
+    return [20];
   }
 };
